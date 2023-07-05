87fac2629633ab6837ca50347fb9cacf3f248b75
zveda21 <zveda.hayrapetyan.97@mail.ru>
2023-07-06 00:22:14 +0400

    added secondary fiels

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KmmFirstApplication"
+include(":androidApp")
+include(":shared")

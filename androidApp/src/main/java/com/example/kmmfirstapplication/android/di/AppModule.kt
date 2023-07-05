c990c7e80f35f4d00f7ed348ba531c89f27c1530
zveda21 <zveda.hayrapetyan.97@mail.ru>
2023-07-06 01:05:31 +0400

    modified AppModule.kt

--- androidApp/src/main/java/com/example/kmmfirstapplication/android/di/AppModule.kt
+++ androidApp/src/main/java/com/example/kmmfirstapplication/android/di/AppModule.kt
@@ -4,7 +4,9 @@ import com.example.kmmfirstapplication.android.presentation.components.detail.De
 import com.example.kmmfirstapplication.android.presentation.components.home.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-
+/**
+ * Created by Zveda Hayrapetyan on 7/5/23.
+ */
 val appModule = module {
     viewModel { HomeViewModel(get()) }
     viewModel { params -> DetailViewModel(getMovieUseCase = get(), movieId = params.get()) }
